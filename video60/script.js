e1264f9b3437ec20222e9fa843f10913fe120870
Abhinaw kumar <>
2026-07-17 20:08:40 +0530

    update

---
+++ video60/script.js
@@ -0,0 +1,9 @@
+console.log("This is string tutorial")
+let a= "Abhinaw"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
