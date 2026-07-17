e148e1b3280187026f442956e11a60bb6b0bffd8
Abhinaw kumar <>
2026-07-17 15:02:09 +0530

    update

---
+++ video55/script.js
@@ -0,0 +1,29 @@
+console.log("This is started video of java")
+var a = 5;
+var b = 7;
+var c = 9;
+console.log(a+b+10)
+console.log(typeof a, typeof b, typeof c)
+/*const a1 = 6;
+a1 =a1+12;
+var c= abhinaw;
+always  use let
+this is not allowd*/
+let p = "Abhinaw";
+let q = 3;
+let  r = 3.33;
+const x = true;
+let y = undefined;
+let z =null;
+console.log(p,q,r,x,y,z)
+console.log( typeof p,typeof q,typeof r,typeof x, typeof y,typeof z)
+let o = {
+    "name":"Abhinaw",
+    "job code":5600,
+    "is_handsome":true
+}
+console.log(o);
+o.salary ="100crores";
+console.log(o);
+o.salary ="500crores";
+console.log(o);
