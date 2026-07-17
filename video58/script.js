4aed70174b11e38e3aadfc68295d1f6615fbdc89
Abhinaw kumar <>
2026-07-17 18:56:47 +0530

    update

---
+++ video58/script.js
@@ -0,0 +1,25 @@
+function nice(name)
+{
+  console.log("Hey"+ name+" you are good") 
+  console.log("Hey "+ name+" you are nice")
+console.log("Hey "+ name+" you are important")
+console.log("Hey  "+ name+" you are rich")
+console.log("Hey "+ name+" you are sssss") 
+}
+nice("Rohan")
+nice("Piyush")
+nice("Abhianw")
+function sum (a, b)
+{
+    //console.log(a+b)
+    return a+b
+}
+result =sum(3 ,9)
+console.log("The sum of number is",result)
+const func1 = (x)=>
+{
+    console.log("The function ", x)
+}
+func1(33);
+func1(66);
+func1(99);
