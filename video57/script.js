1a5cd611d20877b5fae677eb3ff9ef07d9073168
Abhinaw kumar <>
2026-07-17 18:35:17 +0530

    update

---
+++ video57/script.js
@@ -0,0 +1,27 @@
+console.log("I am tutorail on llops")
+let a=1;
+for(let i=0;i<=100;i++)
+{
+    console.log(a+i);
+}
+let obj=
+{
+name:"Harry",
+role:"programer",
+company:"Google",
+}
+for(const key in obj)
+{
+    const element= obj[key];
+    console.log(element)
+}
+for(const c of "Abhinaw")
+{
+    console.log(c)
+}
+let i=0
+while(i<600)
+{
+    console.log(i)
+    i++;
+}
